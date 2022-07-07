79f221f3e87532dd16de9e2f1087ef0fcc5d4de5
AcerRoblox <94796790+AcerRoblox@users.noreply.github.com>
2022-07-07 08:08:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+song = "";
+
+function setup() {
+
+    createCanvas(600, 500);
+    canvas.center();
+}
+p5.js.preload("8d82b5_Pokemon_Theme_Song.mp3").function
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
